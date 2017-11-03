f118c3af3ef5e09b54f4d44d2477dcf2d81b1b37
shogo4405 <shogo4405@gmail.com>
2017-11-03 23:01:24 +0900

    Initial Commit

---
+++ LogboardConsole/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LogboardConsole
+//
+//  Created by Shogo Endo on 2017/11/03.
+//  Copyright © 2017年 Shogo Endo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
